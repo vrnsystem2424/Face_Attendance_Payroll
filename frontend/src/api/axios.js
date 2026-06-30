3845ca88ed9c17d643f2ca04e26191fc7a3f6e2f
avb <avbsoftware.co@gmail.com>
2026-06-30 10:49:24 +0530

    update full done

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -23,7 +23,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  // baseURL: 'http://localhost:5000/api',
+   baseURL: 'https://attendancesystem.up.railway.app/api',
 });
 
 API.interceptors.request.use((req) => {
