d6652e55b3d98b8e3d0f260e7d08a8d3c5ace96c
avb <avbsoftware.co@gmail.com>
2026-09-01 15:19:37 +0530

    update payroll and leave

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,9 +3,11 @@
 import axios from 'axios';
 
 const API = axios.create({
-  // baseURL: 'http://localhost:5001/api',
 
-  baseURL: 'https://attendance-backend-api.signaturesbuilders.com/api',
+  baseURL: 'http://localhost:5001/api',
+
+
+  // baseURL: 'https://attendance-backend-api.signaturesbuilders.com/api',
 });
 
 API.interceptors.request.use((req) => {
